3678852002d253503f1c19a0ecd0cf4bb436caa8
jie.fu <fujie1988@gmail.com>
2020-03-06 01:32:06 +0800

    update run scrips

--- run_train.sh
+++ run_train.sh
@@ -1,4 +1,4 @@
-docker run -it --rm tensorflow/tensorflow:1.14.0-gpu-py3 \
+docker --gpus all run -it --rm tensorflow/tensorflow:1.14.0-gpu-py3 \
  -v `pwd`:/workspace
 export PYTHONPATH="./utils/coco/pycocoevalcap:./utils/coco/pycocoevalcap/bleu:$PYTHONPATH:./utils/coco/pycocoevalcap/cider" && \
 	python main.py --phase=train \

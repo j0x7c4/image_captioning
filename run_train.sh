655c47030d5236877c4f996683aed7a6764d793f
jie.fu <fujie1988@gmail.com>
2020-03-06 01:39:53 +0800

    update

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,7 @@
 docker --gpus all run -it --rm tensorflow/tensorflow:1.14.0-gpu-py3 \
- -v `pwd`:/workspace
-export PYTHONPATH="./utils/coco/pycocoevalcap:./utils/coco/pycocoevalcap/bleu:$PYTHONPATH:./utils/coco/pycocoevalcap/cider" && \
+ -v `pwd`:/workspace \
+ cd /workspace && \
+ export PYTHONPATH="./utils/coco/pycocoevalcap:./utils/coco/pycocoevalcap/bleu:$PYTHONPATH:./utils/coco/pycocoevalcap/cider" && \
 	python main.py --phase=train \
     	--load_cnn \
     	--cnn_model_file='./vgg16_no_fc.npy'\
